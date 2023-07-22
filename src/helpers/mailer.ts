17bfcedb1cd7fbda645dc6b057a78ad33cc9b47f
vsompura3 <vsompura3@gmail.com>
2023-07-22 21:49:03 +0530

    setup mailing service with nodemailer and mailtrap

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,51 @@
+import User from '@/models/user.model'
+import brcypt from 'bcryptjs'
+import nodemailer from 'nodemailer'
+
+export const sendEmail = async ({ email, emailType, userID }: any) => {
+  try {
+    const hashedToken = await brcypt.hash(userID.toString(), 10)
+
+    if (emailType === 'VERIFY') {
+      await User.findByIdAndUpdate(userID, {
+        verifyToken: hashedToken,
+        verifyTokenExpires: Date.now() + 3600000,
+      })
+    } else if (emailType === 'RESET') {
+      await User.findByIdAndUpdate(userID, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpires: Date.now() + 3600000,
+      })
+    }
+
+    const transport = nodemailer.createTransport({
+      host: 'sandbox.smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: '337b8f879930d3',
+        pass: '13b463de4b75bc',
+        // TODO : Add these to env variables
+      },
+    })
+
+    const mailOptions = {
+      from: 'vsompura3@gmail.com',
+      to: email,
+      subject:
+        emailType === 'VERIFY' ? 'Verify your account' : 'Reset password',
+      html: `
+        <h1>Verify your account</h1>
+        <p>Click <a href="${
+          process.env.DOMAIN
+        }/verifyemail?token=${hashedToken}">here</a> to ${
+          emailType === 'VERIFY' ? 'Verify your account' : 'Reset password'
+        }.</p>
+      `,
+    }
+
+    const mailRes = await transport.sendMail(mailOptions)
+    return mailRes
+  } catch (error: any) {
+    throw new Error(error.message)
+  }
+}
